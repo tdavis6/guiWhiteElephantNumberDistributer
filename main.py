b3259e3bc6680d12c9ec1dc49b0bc66225c29f2c
Tyler Davis <coding@tydavis.dev>
2024-08-01 13:59:45 -0700

    fix #10, #7

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ import random
 import json
 import tkinter as tk
 from tkinter import ttk
-from tkinter import messagebox
 
 version = "v0.1.0"
 
@@ -63,26 +62,6 @@ smtpPort = config['smtpPort']
 fromAddress = config['fromAddress']
 smtpPassword = config['smtpPassword']
 
-# main window definitions
-rootWindow = tk.Tk()
-
-window_width = 400
-window_height = 300
-
-# get the screen dimension
-screen_width = rootWindow.winfo_screenwidth()
-screen_height = rootWindow.winfo_screenheight()
-
-# find the center point
-center_x = int(screen_width/2 - window_width / 2)
-center_y = int(screen_height/2 - window_height / 2)
-
-rootWindow.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
-
-name = tk.StringVar()
-primaryEmail = tk.StringVar()
-
-
 #functions
 def clearDB():
     con = sqlite3.connect(dbfile)
@@ -91,9 +70,90 @@ def clearDB():
     con.commit()
     con.close()
     print("Database cleared")
+
+def assignNumbers():
+    con = sqlite3.connect(dbfile)
+    cur = con.cursor()
+    try:
+        data = cur.execute("SELECT * FROM whiteElephantData").fetchall()
+        name, primaryEmail, number = zip(*data)
+    except ValueError:
+        print("No players found")
+    else:
+        assignedNumbers = random.sample(range(len(name)), len(name))
+        assignedNumbers = [x + 1 for x in assignedNumbers]
+
+        for i, name in enumerate(name):
+            cur.execute("UPDATE whiteElephantData SET number = ? WHERE name = ?;", (assignedNumbers[i], name))
+
+        con.commit()
+        con.close()
+
+        print("Numbers assigned")
+
+def emailNumbers():
+    con = sqlite3.connect(dbfile)
+    cur = con.cursor()
+    try:
+        data = cur.execute("SELECT * FROM whiteElephantData").fetchall()
+        name, primaryEmail, number = zip(*data)
+    except ValueError:
+        print("No players found")
+    else:
+        fullNumberList = [(name[i], number[i]) for i in range(len(name))]
+        con.close()
+
+        message = """From: noreply@tylerdavis.net\nTo: {email}\nSubject: White Elephant Number\n\n{name}, your number for White Elephant is {number}!"""
+        messageFullNumberList= """From: noreply@tylerdavis.net\nTo: {email}\nSubject: Full list of White Elephant Numbers\n\nSee below for the full list of numbers:\n{fullNumberList}"""
+
+        context = ssl.create_default_context()
+        with smtplib.SMTP_SSL(smtpServer, smtpPort, context=context) as server:
+            server.login(fromAddress, smtpPassword)
+            server.sendmail(
+                fromAddress,
+                primaryEmail[0],
+                messageFullNumberList.format(email=primaryEmail[0],fullNumberList=fullNumberList)
+            )
+            print("Full Number List sent to " + name[0] + " at " + primaryEmail[0])
+            for i in range(len(name)):
+                server.sendmail(
+                    fromAddress,
+                    primaryEmail[i],
+                    message.format(name=name[i], email=primaryEmail[i], number=number[i])
+                )
+                print("Email sent to " + name[i] + " at " + primaryEmail[i])
+
+def listCurrentPlayers():
+    con = sqlite3.connect(dbfile)
+    cur = con.cursor()
+    try:
+        data = cur.execute("SELECT * FROM whiteElephantData").fetchall()
+        name, primaryEmail, number = zip(*data)
+    except ValueError:
+        print("No players found")
+    else:
+        con.close()
+        for i in range(len(name)):
+            print(name[i])
+
+def openWindow():
+    # remove blur and open main window
+    if rootWindow.state() == "withdrawn":
+        rootWindow.deiconify()
+        print("Main window opened")
+    else:
+        try:
+            from ctypes import windll
+            windll.shcore.SetProcessDpiAwareness(1)
+        except:
+            print("Not a Windows machine, skipping ctypes")
+        finally:
+            rootWindow.mainloop()
+            print("Main window opened")
+
 def onClosing():
+    rootWindow.withdraw()
     print("Main window closed")
-    rootWindow.destroy()
 
 def submitValues():
     name = nameBox.get()
@@ -110,61 +170,26 @@ def submitValues():
     primaryEmailBox.delete(0, tk.END)
     nameBox.focus()
 
-def assignNumbers():
-    con = sqlite3.connect(dbfile)
-    cur = con.cursor()
-    data = cur.execute("SELECT * FROM whiteElephantData").fetchall()
-    name, primaryEmail, number = zip(*data)
+# main window definition
+rootWindow = tk.Tk()
 
-    assignedNumbers = random.sample(range(len(name)), len(name))
-    assignedNumbers = [x + 1 for x in assignedNumbers]
+rootWindow.protocol("WM_DELETE_WINDOW", onClosing)
 
-    for i, name in enumerate(name):
-        cur.execute("UPDATE whiteElephantData SET number = ? WHERE name = ?;", (assignedNumbers[i], name))
+window_width = 400
+window_height = 300
 
-    con.commit()
-    con.close()
+# get the screen dimension
+screen_width = rootWindow.winfo_screenwidth()
+screen_height = rootWindow.winfo_screenheight()
 
-    print("Numbers assigned")
+# find the center point
+center_x = int(screen_width/2 - window_width / 2)
+center_y = int(screen_height/2 - window_height / 2)
 
-def emailNumbers():
-    con = sqlite3.connect(dbfile)
-    cur = con.cursor()
-    data = cur.execute("SELECT * FROM whiteElephantData").fetchall()
-    name, primaryEmail, number = zip(*data)
-    fullNumberList = [(name[i], number[i]) for i in range(len(name))]
-    con.close()
+rootWindow.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
 
-    message = """From: noreply@tylerdavis.net\nTo: {email}\nSubject: White Elephant Number\n\n{name}, your number for White Elephant is {number}!"""
-    messageFullNumberList= """From: noreply@tylerdavis.net\nTo: {email}\nSubject: Full list of White Elephant Numbers\n\nSee below for the full list of numbers:\n{fullNumberList}"""
-
-    context = ssl.create_default_context()
-    with smtplib.SMTP_SSL(smtpServer, smtpPort, context=context) as server:
-        server.login(fromAddress, smtpPassword)
-        server.sendmail(
-            fromAddress,
-            primaryEmail[0],
-            messageFullNumberList.format(email=primaryEmail[0],fullNumberList=fullNumberList)
-        )
-        print("Full Number List sent to " + name[0] + " at " + primaryEmail[0])
-        for i in range(len(name)):
-            server.sendmail(
-                fromAddress,
-                primaryEmail[i],
-                message.format(name=name[i], email=primaryEmail[i], number=number[i])
-            )
-            print("Email sent to " + name[i] + " at " + primaryEmail[i])
-
-def listCurrentPlayers():
-    con = sqlite3.connect(dbfile)
-    cur = con.cursor()
-    data = cur.execute("SELECT * FROM whiteElephantData").fetchall()
-    name, primaryEmail, number = zip(*data)
-    con.close()
-
-    for i in range(len(name)):
-        print(name[i])
-
+name = tk.StringVar()
+primaryEmail = tk.StringVar()
 
 rootWindow.title("White Elephant Number Distributor")
 
@@ -189,46 +214,47 @@ submitButton = ttk.Button(
 )
 submitButton.pack(padx=10, pady=10, fill='x', expand=True)
 
-def openWindow():
-    # remove blur and open main window
-    try:
-        from ctypes import windll
-        windll.shcore.SetProcessDpiAwareness(1)
-    except:
-        print("Not a Windows machine, skipping ctypes")
-    finally:
-        rootWindow.protocol("WM_DELETE_WINDOW", onClosing)
-        rootWindow.mainloop()
+rootWindow.withdraw()
 
-menu = {}
-menu['1']="Gather new players"
-menu['2']="List current players"
-menu['3']="Assign and email numbers"
-menu['4']="Clear DB"
-menu['5']="Exit"
+def printMenu():
+    menu = {}
+    menu['1'] = "Gather new players"
+    menu['2'] = "List current players"
+    menu['3'] = "Assign and email numbers"
+    menu['4'] = "Clear DB"
+    menu['5'] = "Print Menu"
+    menu['6'] = "Exit"
 
-while True:
     options=menu.keys()
-    print()
     for entry in options:
         print(entry, menu[entry])
 
-    print()
+print()
+printMenu()
+
+while True:
     selection=input("Please select an option:")
     if selection =='1':
         print()
         openWindow()
+        print()
     elif selection =='2':
         print()
         listCurrentPlayers()
+        print()
     elif selection =='3':
         print()
         assignNumbers()
         emailNumbers()
+        print()
     elif selection =='4':
         print()
         clearDB()
+        print()
     elif selection =='5':
+        print()
+        printMenu()
+    elif selection =='6':
         break
     else:
-        print("Invalid selection")
+        print("\nInvalid selection\n")
